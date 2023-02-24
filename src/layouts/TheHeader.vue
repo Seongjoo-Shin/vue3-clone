da498c157b1227d1fc26b6d421a2025b291ee92e
Shin Seongjoo <tloc0e04@osstem.com>
2023-02-24 13:31:43 +0900

    g2

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -7,6 +7,18 @@
           href="#"
           >MUSINSA</a
         >
+        <ul class="navbar-brand none-decorator mt-3">
+          <li style="color: green">스웨트</li>
+          <li>랭킹</li>
+          <li>업데이트</li>
+          <li>코디</li>
+          <li>세일</li>
+          <li>스페셜</li>
+          <li>매거진</li>
+          <li>TV</li>
+          <li>이벤트</li>
+          <li>브랜드</li>
+        </ul>
       </div>
     </nav>
   </header>
@@ -14,4 +26,14 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+}
+li {
+  float: left;
+  font-size: 16px;
+  margin-left: 15px;
+  font-weight: 600;
+}
+</style>
